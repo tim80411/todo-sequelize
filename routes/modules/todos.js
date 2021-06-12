00a030b59ed34d8eef192658a0f9edb8c70fb034
Yiting Liao <tim80411@gmail.com>
2021-06-12 21:12:45 +0800

    feat: add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -5,6 +5,29 @@ const db = require('../../models')
 const User = db.User
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', async (req, res) => {
+  const userId = req.user.id
+  const name = req.body.name
+
+  try {
+    await Todo.create({
+      name,
+      UserId: userId,
+    })
+    
+    return res.redirect('/')
+  } catch (error) {
+    req.flash('error', '資料庫載入失敗，請稍後再試')
+
+    res.redirect('/')
+  }
+
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
