cf9efed99b76df201b4b2ca5394eb194dc6e16fa
Yiting Liao <tim80411@gmail.com>
2021-06-12 21:26:58 +0800

    feat: add upadte todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
       name,
       UserId: userId,
     })
-    
+
     return res.redirect('/')
   } catch (error) {
     req.flash('error', '資料庫載入失敗，請稍後再試')
@@ -35,4 +35,39 @@ router.get('/:id', (req, res) => {
     .catch(err => console.log(err))
 })
 
+router.get('/:id/edit', async (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  try {
+    let todo = await Todo.findOne({ id, UserId: userId })
+    todo = todo.toJSON()
+
+    res.render('edit', { todo })
+  } catch (error) {
+    req.flash('error', '資料庫載入失敗，請稍後再試')
+
+    res.redirect('/')
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const userId = req.user._id
+  const id = req.params.id
+  const { name, isDone } = req.body
+
+  try {
+    const todo = await Todo.findOne({ id, UserId: userId })
+    todo.name = name
+    todo.isDone = isDone === 'on'
+
+    await todo.save()
+
+    return res.redirect(`/todos/${id}`)
+
+  } catch (error) {
+    req.flash('error', '資料庫載入失敗，請稍後再試')
+
+    res.redirect('/')
+  }
+})
 module.exports = router
