964914a24d5609f49d2a1a78f3d056c16c71dd50
Yiting Liao <tim80411@gmail.com>
2021-06-12 15:12:35 +0800

    feat: add index page and modify router

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then(todos => {
+      return res.render('index', { todos })
+    })
+    .catch(error => {
+      return res.status(422).json(error)
+    })
 })
 
 app.get('/users/login', (req, res) => {
